ae0db08bed8b67f92a72b57f6543ceb92bb652d0
feng.fan <feng.fan@kyligence.io>
2025-03-25 17:34:01 +0800

    fix [Parameter 1 of constructor in io.kyligence.kap.gateway.route.KylinRefreshRouteTableScheduler required a bean of type 'org.sp      ringframework.cloud.gateway.actuate.AbstractGatewayControllerEndpoint' that could not be found.]

--- kylin-gateway/src/main/java/io/kyligence/kap/gateway/route/KylinRefreshRouteTableScheduler.java
+++ kylin-gateway/src/main/java/io/kyligence/kap/gateway/route/KylinRefreshRouteTableScheduler.java
@@ -38,8 +38,10 @@ public class KylinRefreshRouteTableScheduler implements ApplicationEventPublishe
 
 	protected ApplicationEventPublisher publisher;
 
+	@Autowired
 	private AbstractGatewayControllerEndpoint gatewayControllerEndpoint;
 
+	@Autowired
 	private LoadBalancerClientFilter loadBalancerClientFilter;
 
 	private IRouteTableReader routeTableReader;
